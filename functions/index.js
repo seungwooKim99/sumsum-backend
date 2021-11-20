04f3dd992ab46373d93e9940f41f7f90822738fb
Seungwoo Kim <sonicdx886@gmail.com>
2021-11-21 02:21:00 +0900

    ➕ dotenv 설치 및 설정

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,16 @@ import express from "express";
 import admin from 'firebase-admin';
 import loaders from './src/loaders/index.js';
 
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-  //admin.initializeApp();
-  const app = loaders();
+//admin.initializeApp();
+const app = loaders();
 
 
 export const api = functions.https.onRequest(app);
