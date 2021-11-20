97aed11271c37af09cd4fdb84cae26197c5efe0c
Seungwoo Kim <sonicdx886@gmail.com>
2021-11-21 01:08:02 +0900

    ✨ 카카오 로그인 이후 회원가입 api 개발

--- functions/src/services/authService.js
+++ functions/src/services/authService.js
@@ -61,33 +61,54 @@ export default {
         console.log('Got Response from kakao user check API. and the response is…')
         console.log(body)
         const userId = `kakao:${body.id}`
+        //const userId = body.id
         if (!userId) {
           return res.status(404)
           .send({message: 'There was no user with the given access token.'})
         }
-        //let nickname = null
-        let nickname = 'seungwookim'
+        let nickname = null
         let profileImage = null
         if (body.properties) {
           nickname = body.properties.nickname
           profileImage = body.properties.profile_image
         }
-        return updateOrCreateUser(userId, body.kaccount_email, nickname,profileImage)
+        return updateOrCreateUser(userId, body.kakao_account.email, nickname,profileImage)
         .then((userRecord) => {
           const userId = userRecord.uid
+          console.log(userRecord)
           console.log(`creating a custom firebase token based on uid ${userId}`)
-          admin.auth().createCustomToken(userId)
-            .then((customToken) => {
-              console.log(customToken)
-              res.status(200).send(customToken);
+
+          //createUser()
+
+          const user = {
+            uid : userRecord.uid,
+            name : userRecord.displayName,
+            nickname : null,
+            email : userRecord.email || null,
+            isPhoneAuthDone : false,
+            isUnivAuthDone : false,
+            isNicknameSettingDone : false,
+            isPosted : false,
+            wishLocation : null,
+            xLocation : null,
+            yLoaction : null,
+          }
+        
+          db.collection('users').add(user)
+            .then((response)=>{
+              admin.auth().createCustomToken(userId)
+                .then((customToken) => {
+                  console.log(customToken)
+                  res.status(200).send(customToken);
+                })
+                .catch((error) => {
+                  console.log(error)
+                })
             })
             .catch((error) => {
               console.log(error)
             })
         })
-
-
-        //
       })
       .catch((error) => {
         console.log(error.message)
