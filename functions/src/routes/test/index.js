88d2b6590d4f50ce95768c80c97751d4e38b6333
Seungwoo Kim <sonicdx886@gmail.com>
2021-11-21 18:06:06 +0900

    🧪 test 코드

---
+++ functions/src/routes/test/index.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+
+const router = express.Router()
+
+const generateToken = (req,res) =>{
+
+  const {uid} = req.body
+  const token = jwt.sign({ uid }, process.env.TOKEN_SECRET, {
+    expiresIn: '90d',
+    issuer: 'sumsum',
+  })
+  res.status(200).send(token)
+}
+
+router.get('/verifytoken', (req,res) => {
+    // 클라이언트는 header 내부에 'Authorization' : 'Bearer <token...>' 형태로 토큰을 전달한다. ('key': 'value' 쌍)
+    const authHeader = req.headers.authorization
+
+    // authHeader는 'Bearer <token...>'이다.
+    if (!authHeader) {
+      console.log('not logged in!!')
+      return res.status(403).json({
+        success: false,
+        statusCode: 403,
+        message: 'not logged in',
+      })
+    }
+
+    // 'Bearer', '<token...>'중 '<token...>'만 받아서 저장한다.
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        console.log('Wrong token!')
+        return res.status(403).json({
+          success: false,
+          statusCode: 403,
+          message: err.message,
+        })
+      }
+      // req.decoded에 토큰을 decode한 내용을 저장해 넘겨준다.
+      // req.decoded 내부에는 trainerid, _id, expiresIn 등 토큰에 대한 정보다 담겨있다.
+      res.status(200).send(decoded)
+    })
+});
+
+router.post('/gentoken', generateToken)
+
+export default router
