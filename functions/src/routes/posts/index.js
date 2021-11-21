e1f3d9007ddf41a0aa2d33e5565ba97e91b94194
Seungwoo Kim <sonicdx886@gmail.com>
2021-11-21 22:05:18 +0900

    ✨ Post 조회, 생성 기능 추가

--- functions/src/routes/posts/index.js
+++ functions/src/routes/posts/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import verifyTokenMiddleware from '../../middlewares/verifytoken.js';
 import postsService from '../../services/postsService.js'
 
 const router = express.Router()
 
 router.get('/', postsService.getPosts);
-router.post('/', postsService.createPost);
+router.get('/:id', postsService.getPostByUid);
+router.post('/', verifyTokenMiddleware, postsService.createPost);
+//router.post('/filter', postsService.createPost);
 
 export default router
